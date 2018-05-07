26316f146dbc19286ebfa80c86b752ea5e9df4dc
clm110 <275298711@qq.com>
2018-05-07 10:33:31 +0800

    手动实现获取用户信息

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var OAuth = require('wechat-oauth');
 var fs = require('fs')
+var superagent = require('superagent')
 // var client = new OAuth('your appid', 'your secret');
 var client = new OAuth('wx75340481908402a8', '2b6ee0cbeec0114eb539e68ba356329b', function (openid, callback) {
 
@@ -26,17 +27,35 @@ router.get('/feauth', function (req, res) {
     res.redirect(FEurl)
 });
 
+// router.get('/code', function (req, res) {
+//     let code = req.query.code;  
+//     client.getAccessToken(code, function (err, result) {
+//         var accessToken = result.data.access_token;
+//         var openid = result.data.openid;
+//         // res.send(openid)
+//         client.getUser(openid, function (err, result) {
+//             var userInfo = result;
+//             res.json(userInfo)
+//           });
+//       });
+// });
+
 router.get('/code', function (req, res) {
-    let code = req.query.code;  
-    client.getAccessToken(code, function (err, result) {
-        var accessToken = result.data.access_token;
-        var openid = result.data.openid;
-        // res.send(openid)
-        client.getUser(openid, function (err, result) {
-            var userInfo = result;
-            res.json(userInfo)
-          });
-      });
+    let CODE = req.query.code; 
+    var APPID = "wx75340481908402a8"
+    var SECRET = "2b6ee0cbeec0114eb539e68ba356329b"
+    var url1 = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${APPID}&secret=${SECRET}&code=${CODE}&grant_type=authorization_code` 
+    superagent.get(url1).end(function(err,response){
+        console.log(JSON.parse(response.text));
+        var ACCESS_TOKEN = JSON.parse(response.text).access_token;
+        var OPENID = JSON.parse(response.text).openid;
+        var url2 = `https://api.weixin.qq.com/sns/userinfo?access_token=${ACCESS_TOKEN}&openid=${OPENID}&lang=zh_CN`;
+        superagent.get(url2).end(function(err,response){
+            res.send(response.text)
+        })
+    })
 });
+
+
 
 module.exports = router;
